877f0449fbf1bc80b096c7bc14ad7a043f26e06b
lcr-thiago-fernandes <thiago.fernandes@lucere.com.br>
2025-02-21 15:45:18 -0300

    feat: adicionar entidades Sale e SaleItem com mapeamento e migrações
    - Criadas as entidades Sale e SaleItem
    - Configurado o mapeamento das entidades com Entity Framework Core
    - Gerada e aplicada a migração para atualização do banco de dados

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.RegularExpressions;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+{
+    public void Configure(EntityTypeBuilder<Sale> builder)
+    {
+        builder.ToTable("Sales");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(u => u.SaleNumber)
+            .IsRequired()
+            .HasColumnType("varchar(50)");
+
+        builder.Property(u => u.SaleDate)
+            .IsRequired()
+            .HasColumnType("timestamp");
+
+        builder.Property(u => u.Customer)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.Property(u => u.TotalAmount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(u => u.Branch)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.Property(u => u.IsCancelled)
+            .IsRequired()
+            .HasColumnType("boolean")
+            .HasDefaultValue(false);
+
+        builder.HasMany(s => s.Items)
+            .WithOne()
+            .HasForeignKey(si => si.SaleId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.RegularExpressions;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(u => u.SaleId)
+            .IsRequired()
+            .HasColumnType("uuid");
+
+        builder.Property(u => u.Product)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.Property(u => u.Quantity)
+            .IsRequired()
+            .HasColumnType("int");
+
+        builder.Property(u => u.UnitPrice)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(u => u.Discount)
+            .IsRequired()
+            .HasColumnType("decimal(5,2)")
+            .HasDefaultValue(0);
+
+        builder.Property(u => u.TotalAmount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.HasOne<Sale>()
+            .WithMany(s => s.Items)
+            .HasForeignKey(u => u.SaleId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
